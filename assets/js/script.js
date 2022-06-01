a5876d97d87aab5007867426f792e6de4b6f787c
Jared Daniel <jareddaniel95@gmail.com>
2022-05-31 22:47:13 -0400

    Add current day section, formatting and textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,43 @@
 // For loop that runs from 9AM to 5PM (9 total blocks) compare time and set class to past if before current time
 
+var currentDayElement = $('#currentDay');
 var blocksElement = $('#blocks');
-// var now = moment().format("h:mm:ss a");
-// var now24 = moment().format("H:mm:ss");
-//test
-var now = moment("3:12:07 PM", "h:mm:ss a");
-var now24 = moment("15:12:07", "H:mm:ss");
+var today = moment().format("dddd, MMMM Do");
+// var now = moment().format("H:mm:ss");
+
+// for testing
+// var today = moment("Wednesday, June 1st 15:12:07", "dddd, MMMM Do H:mm:ss");
+var now = moment("15:12:07", "H:mm:ss");
+
+currentDayElement.text(today);
 
 for (var i = 9; i < 18; ++i) {
     var timeBlock = $('<div>');
-    timeBlock.addClass('row');
+    timeBlock.addClass('time-block');
+    /* HOUR DIV */
+    var hourDiv = $('<div>');
+    hourDiv.addClass('hour');
     timeIndex = moment(`${i}:00`, 'H:mm');
-    if (timeIndex.hour() === now24.hour()) {
-        timeBlock.addClass('present');
-    } else if (timeIndex.isBefore(now24)) {
-        timeBlock.addClass('past');
-    } else if (timeIndex.isAfter(now24)) {
-        timeBlock.addClass('future');
+    hourDiv.text(moment(timeIndex, "H:mm").format("hA"));
+    
+    /* TEXT DIV */
+    var textDiv = $('<div>');
+    textDiv.addClass('textarea');
+
+    if (timeIndex.hour() === now.hour()) {
+        textDiv.addClass('present');
+    } else if (timeIndex.isBefore(now)) {
+        textDiv.addClass('past');
+    } else if (timeIndex.isAfter(now)) {
+        textDiv.addClass('future');
     }
 
-    timeBlock.text(moment(timeIndex, "H:mm").format("hA"));
+    var textArea = $('<textarea style="width:100%, height:100%"/>');
+    textDiv.append(textArea);
     
+    timeBlock.append(hourDiv);
+    timeBlock.append(textDiv);
+
     blocksElement.append(timeBlock);
 }
 
-// 9, 10, 11, 12, 1, 2, 3 <> 3:12 <> 4, 5
