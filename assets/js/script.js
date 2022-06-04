0e59c0f6b10d23dc5047a587bd90ad4426041210
Jared Daniel <jareddaniel95@gmail.com>
2022-06-03 21:08:02 -0400

    Add save and load functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-// For loop that runs from 9AM to 5PM (9 total blocks) compare time and set class to past if before current time
-
 var currentDayElement = $('#currentDay');
 var blocksElement = $('#blocks');
 var today = moment().format("dddd, MMMM Do");
-// var now = moment().format("H:mm:ss");
+var now = moment();//.format("H:mm:ss");
 
 // for testing
-var now = moment("13:12:07", "H:mm:ss");
+// var now = moment("13:12:07", "H:mm:ss");
 
 currentDayElement.text(today);
 
@@ -17,7 +15,9 @@ for (var i = 9; i < 18; ++i) {
     var hourDiv = $('<div>');
     hourDiv.addClass('hour');
     timeIndex = moment(`${i}:00`, 'H:mm');
-    hourDiv.text(moment(timeIndex, "H:mm").format("hA"));
+    intHour = timeIndex.hour();
+    var rowData = moment(timeIndex, "H:mm").format("hA");
+    hourDiv.text(rowData);
     
     /* TEXT DIV */
     var timeBlockDiv = $('<div>');
@@ -26,7 +26,7 @@ for (var i = 9; i < 18; ++i) {
     textDiv.addClass('textarea');
     
 
-    if (timeIndex.hour() === now.hour()) {
+    if (intHour === now.hour()) {
         textDiv.addClass('present');
     } else if (timeIndex.isBefore(now)) {
         textDiv.addClass('past');
@@ -37,10 +37,15 @@ for (var i = 9; i < 18; ++i) {
     var textArea = $('<textarea style="width:100%, height:100%"/>');
     textDiv.append(textArea);
     timeBlockDiv.append(textDiv);
-    
+    var savedText = localStorage.getItem(rowData);
+    if (savedText != null) {
+        textArea.val(savedText);
+    }
+
     /* SAVE DIV */
     var saveDiv = $('<div>');
     saveDiv.addClass('saveBtn');
+    saveDiv.data("hour", rowData);
     saveDiv.html('&#128190;');
 
     rowDiv.append(hourDiv);
@@ -49,3 +54,7 @@ for (var i = 9; i < 18; ++i) {
 
     blocksElement.append(rowDiv);
 }
+
+blocksElement.on('click', '.saveBtn', function() {
+    localStorage.setItem($(this).data("hour"), $(this).parent('div').find('.time-block').children().first().children().first().val());
+});
